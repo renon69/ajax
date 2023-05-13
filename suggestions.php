e239ea03a5204b3845df08fa6b0efa4a3ea33a06
renon <sugitarios.renon_vincent@hnu.edu.ph>
2023-05-13 14:53:52 +0800

    ajax

--- suggestions.php
+++ suggestions.php
@@ -1,14 +1,17 @@
 <?php 
-    $existingNames = ["renon", "vincent",];
+    $existingNames = ["renon", "vincent","rayallen", "rui"];
 
     if(isset($_POST['suggestion'])) {
         $name = $_POST['suggestion'];
-
+        
+        if(!empty($name)) {
+            
         foreach ($existingNames as $existingName) {
             if(strpos($existingName, $name) !== false) {
                 echo $existingName;
                 echo "<br>";
             }
          }
+        }
     }
 ?>
